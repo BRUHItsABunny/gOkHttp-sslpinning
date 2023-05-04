76f62a33dc818032bf6e24f76ede633b3c09d257
BRUHITsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2023-05-04 03:06:01 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/BRUHItsABunny/gOkHttp-sslpinning
+
+go 1.19
+
+require (
+	github.com/BRUHItsABunny/crypto-utils v0.0.5
+	github.com/BRUHItsABunny/gOkHttp v0.2.9
+	github.com/cornelk/hashmap v1.0.8
+)
+
+require github.com/OneOfOne/xxhash v1.2.8 // indirect
---
+++ sslpinning_test.go
@@ -0,0 +1,24 @@
+package gokhttp_sslpinning
+
+import (
+	"context"
+	gokhttp "github.com/BRUHItsABunny/gOkHttp"
+	gokhttp_requests "github.com/BRUHItsABunny/gOkHttp/requests"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNewSSLPinningOption(t *testing.T) {
+	pinner := NewSSLPinningOption()
+	err := pinner.AddPin("github.com", true, "sha256\\/3ftdeWqIAONye/CeEQuLGvtlw4MPnQmKgyPLugFbK8=")
+	require.NoError(t, err, "pinner.AddPin: errored unexpectedly.")
+
+	hClient, err := gokhttp.NewHTTPClient(pinner)
+	require.NoError(t, err, "NewHTTPClient: errored unexpectedly.")
+
+	req, err := gokhttp_requests.MakeGETRequest(context.Background(), "https://github.com")
+	require.NoError(t, err, "requests.MakeGETRequest: errored unexpectedly.")
+
+	_, err = hClient.Do(req)
+	require.NoError(t, err, "hClient.Do: errored unexpectedly.")
+}
